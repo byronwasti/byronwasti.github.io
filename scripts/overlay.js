dbfcf033ca998321de08384ba6d7d5168a0a78b3
byronwasti <byron.wasti@gmail.com>
2016-10-10 15:20:49 -0400

    Website.

--- scripts/overlay.js
+++
@@ -1,8 +0,0 @@
-console.log("test");
-
-function runscript(e) {
-  console.log("clicked");
-  console.log(e);
-  let overlay = e.id + '-1';
-  document.getElementById(overlay).style.display = 'block';
-}
